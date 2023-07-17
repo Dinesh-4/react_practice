96be17bfb68813b5bcdb91187bd653e18293bdff
Dinesh-4 <dinesh46.mv@gmail.com>
2023-07-17 22:22:22 +0530

    Passing Data via Props

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,21 +1,28 @@
 import { useState } from "react";
 
-function ListGroup() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+// { items: [], heading: string }
+interface Props{
+  items: string[];
+  heading: string;
+};
+
+function ListGroup({items, heading}: Props) {
+  
   
   //Hook - data that will change over time
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
-      <h1>List Group</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
           <li 
             className={
               selectedIndex === index
-                ? "list-group-item active"
+                ? "list-group-item active" 
                 : "list-group-item"
             }
             key={item}
