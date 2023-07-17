4e28213381331f8dccd3707e6404a5aab10daeab
Dinesh-4 <dinesh46.mv@gmail.com>
2023-07-17 14:21:29 +0530

    Conditional Rendering

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,12 +1,29 @@
 function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
+  // if (items.length === 0)
+  //   return (
+  //     <>
+  //       <h1>List</h1>
+  //       <p>No item found</p>
+  //     </>
+  //   );
+  // const message = items.length === 0 ? <p>No item found</p> : null;
 
+  // const getMessage = () => {
+  //   return items.length === 0 ? <p>No item found</p> : null;
+  // }
   return (
     <>
       <h1>List Group</h1>
+      {/* {items.length === 0 ? <p>No item found</p> : null} */}
+      {/* { message } */}
+      {/* {getMessage()} */}
+      {/* { items.length === 0 ? <p>No item found</p> : null } */}
+      {items.length === 0 && <p>No item found</p> }
       <ul className="list-group">
         {items.map((item) => (
-          <li key={item }>{item}</li>
+          <li key={item}>{item}</li>
         ))}
       </ul>
     </>
