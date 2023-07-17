75bdefdb91f0340024d142f7778bb78a04f723e1
Dinesh-4 <dinesh46.mv@gmail.com>
2023-07-17 15:07:20 +0530

    Event Handler

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,29 +1,25 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  items = [];
-  // if (items.length === 0)
-  //   return (
-  //     <>
-  //       <h1>List</h1>
-  //       <p>No item found</p>
-  //     </>
-  //   );
-  // const message = items.length === 0 ? <p>No item found</p> : null;
 
-  // const getMessage = () => {
-  //   return items.length === 0 ? <p>No item found</p> : null;
-  // }
+
+  //Event handler
+  const handleClick = (event : MouseEvent) => console.log(event.type)
   return (
     <>
       <h1>List Group</h1>
-      {/* {items.length === 0 ? <p>No item found</p> : null} */}
-      {/* { message } */}
-      {/* {getMessage()} */}
-      {/* { items.length === 0 ? <p>No item found</p> : null } */}
-      {items.length === 0 && <p>No item found</p> }
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            // onClick={(event) => console.log(event)}
+            onClick={handleClick}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
