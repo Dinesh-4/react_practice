b3ac96d4504e313a410b86c9b6b86ef34cc8b7ad
Dinesh-4 <dinesh46.mv@gmail.com>
2023-07-19 21:02:38 +0530

    Updating Objects

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,14 +1,26 @@
 import { useState } from "react";
-import Message from "./Message";
 
 function App() {
+  const [drink, setDrink] = useState({
+    title: "Americano",
+    price: 5,
+  });
+  
 
+  const handleClick = () => {
+    // const newDrink = {
+    //   // title: drink.title,
+    //   ...drink,
+    //   price: 6
+    // }
+    // setDrink(newDrink);
+
+    setDrink({ ...drink, price: 6 });
+  };
   return (
     <div>
-      <Message />
-      <Message />
-      <Message />
-
+      {drink.price}
+      <button onClick={handleClick}>Click Me</button>
     </div>
   );
 }
