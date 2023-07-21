f7914e8c6e8aaa49fa2051dba8ab1bd9c80d3f3b
Dinesh-4 <dinesh46.mv@gmail.com>
2023-07-21 22:35:18 +0530

    Building an ExpandableText Component

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,21 +1,11 @@
-import { useState } from "react";
+import ExpandableText from "./components/ExpandableText";
 
 function App() {
-  const [cart, setCard] = useState({
-    discount: .1,
-    items: [
-      {id: 1, tittle: 'Product 1', quantity: 1},
-      {id: 1, tittle: 'Product 1', quantity: 1}
-    ]
-  });
-
-  const handleClick = () => {
-    setCard({ ...cart, items: cart.items.map(item => item.id === 1 ? { ...item, quantity: item.quantity +1 } : item)})
-  } 
-
   return (
     <div>
-      <button onClick={handleClick}>Click Me</button>
+      <ExpandableText>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur laudantium natus voluptates perspiciatis! A nesciunt distinctio voluptatibus veniam fuga eaque alias, excepturi dicta accusamus soluta veritatis eos minus incidunt vitae dignissimos. Veniam culpa accusantium iste, animi repellat similique eius beatae ducimus corrupti explicabo. Rerum atque quibusdam reiciendis quam blanditiis nobis perspiciatis excepturi, culpa cumque iste magni perferendis ipsam voluptates debitis eius velit molestiae accusantium aperiam numquam ipsa repellat temporibus! Et, voluptatem. Repellendus, deserunt rem omnis illum hic dolorum nisi accusantium placeat itaque odio sunt eaque enim at, consequatur earum nobis ea labore asperiores eum sapiente laudantium laborum harum? Animi, repellat
+      </ExpandableText>
     </div>
   );
 }
