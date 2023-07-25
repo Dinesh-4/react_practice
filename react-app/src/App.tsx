cfdd2bfcf6730bb2b6ffbe81cc20d766ebc68d8d
Dinesh-4 <dinesh46.mv@gmail.com>
2023-07-25 18:59:03 +0530

    Building a Form

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,11 +1,9 @@
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div>
-      <ExpandableText>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur laudantium natus voluptates perspiciatis! A nesciunt distinctio voluptatibus veniam fuga eaque alias, excepturi dicta accusamus soluta veritatis eos minus incidunt vitae dignissimos. Veniam culpa accusantium iste, animi repellat similique eius beatae ducimus corrupti explicabo. Rerum atque quibusdam reiciendis quam blanditiis nobis perspiciatis excepturi, culpa cumque iste magni perferendis ipsam voluptates debitis eius velit molestiae accusantium aperiam numquam ipsa repellat temporibus! Et, voluptatem. Repellendus, deserunt rem omnis illum hic dolorum nisi accusantium placeat itaque odio sunt eaque enim at, consequatur earum nobis ea labore asperiores eum sapiente laudantium laborum harum? Animi, repellat
-      </ExpandableText>
+      <Form />
     </div>
   );
 }
